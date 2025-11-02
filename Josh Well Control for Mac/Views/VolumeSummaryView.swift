ce67e46681503b048fb37daf2f0dee7f014929c1
joshsallows <joshsallows@gmail.com>
2025-11-02 15:03:49 -0700

    a nice volume summary view

---
+++ Josh Well Control for Mac/Views/VolumeSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  VolumeSummaryView.swift
+//  Josh Well Control for Mac
+//
+//  Created by Josh Sallows on 2025-11-02.
+//
+
+import SwiftUI
+
+struct VolumeSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VolumeSummaryView()
+}
